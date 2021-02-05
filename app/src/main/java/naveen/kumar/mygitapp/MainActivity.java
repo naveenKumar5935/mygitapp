bbf5ca15dc6ce1450a889a152c7d897b4ae7378c
naveenkumar <naveenbti002@gmail.com>
2021-02-06 02:39:10 +0530

    done

---
+++ app/src/main/java/naveen/kumar/mygitapp/MainActivity.java
@@ -0,0 +1,14 @@
+package naveen.kumar.mygitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
